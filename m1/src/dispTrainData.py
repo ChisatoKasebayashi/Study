0ee45322d6f5e020e5a54291aeb9ac9c41e8aa78
ChisatoKasebayashi <chisato.rukia@gmail.com>
2018-08-27 19:26:16 +0900

    整理

--- m1/src/dispTrainData.py
+++ m1/src/dispTrainData.py
@@ -24,7 +24,6 @@ g_ball = np.array(data.iloc[:,6:8])
 g_pole0 = np.array(data.iloc[:,8:10])
 g_pole1 = np.array(data.iloc[:,10:12])
 g_robot = np.array(data.iloc[:,12:15])
-print(l_ball)
 
 fig, ax = plt.subplots()
 fig.set_tight_layout(True)
@@ -39,6 +38,17 @@ def convertPoint(x, y, im_w, im_h):
     y = (y/6.0)*im_h+(im_h/2)+70
     return x,y
 
+def calcObjectPosition(ball, robot, pole0, pole1, im_w, im_h,step):
+    print(ball)
+    print(ball[0])
+    print(ball[1])
+    bx, by = convertPoint(ball[0],ball[1],im_w, im_h)
+    rx, ry = convertPoint(robot[0],robot[1],im_w, im_h)
+    p0x, p0y = convertPoint(pole0[0],pole0[1],im_w, im_h)
+    p1x, p1y = convertPoint(pole1[0],pole1[1],im_w, im_h)
+    return [bx, by, rx, ry, p0x, p0y, p1x, p1y]
+
+
 def update(i):
     if i !=0:
         plt.cla()
@@ -49,22 +59,27 @@ def update(i):
     ax.set_ylim(0,740)
     ax.imshow(nimg)
     step = i*10
+    print('if no mae no'+str(step))
     if step < len(l_ball):
 #        lbx, lby =  convertPoint(robot[step][0]+l_ball[step][0],robot[step][1]+l_ball[step][1],im_w, im_h)
         #g_ball = tools.geometry.coord_trans_local_to_global(robot[step],l_ball[step])
-        bx, by = convertPoint(g_ball[step][0],g_ball[step][1],im_w, im_h)
-        rx, ry = convertPoint(g_robot[step][0],g_robot[step][1],im_w, im_h)
-        p0x, p0y = convertPoint(g_pole0[step][0],g_pole0[step][1],im_w, im_h)
-        p1x, p1y = convertPoint(g_pole1[step][0],g_pole1[step][1],im_w, im_h)
+        #bx, by = convertPoint(g_ball[step][0],g_ball[step][1],im_w, im_h)
+        #rx, ry = convertPoint(g_robot[step][0],g_robot[step][1],im_w, im_h)
+        #p0x, p0y = convertPoint(g_pole0[step][0],g_pole0[step][1],im_w, im_h)
+        #p1x, p1y = convertPoint(g_pole1[step][0],g_pole1[step][1],im_w, im_h)
+        #print(g_ball[step])
+        bx, by, rx, ry, p0x, p0y, p1x, p1y = calcObjectPosition(g_ball[step],g_robot[step],g_pole0[step],g_pole1[step],im_w,im_h,step)
+
 
     else:
         step = len(l_ball)-1
 #        lbx, lby =  convertPoint(robot[step][0]+l_ball[step][0],robot[step][1]+l_ball[step][1],im_w, im_h)
-        bx, by = convertPoint(g_ball[step][0],g_ball[step][1],im_w, im_h)
-        rx, ry = convertPoint(g_robot[step][0],g_robot[step][1],im_w, im_h)
-        p0x, p0y = convertPoint(g_pole0[step][0],g_pole0[step][1],im_w, im_h)
-        p1x, p1y = convertPoint(g_pole1[step][0],g_pole1[step][1],im_w, im_h)
-    
+        #bx, by = convertPoint(g_ball[step][0],g_ball[step][1],im_w, im_h)
+        #rx, ry = convertPoint(g_robot[step][0],g_robot[step][1],im_w, im_h)
+        #p0x, p0y = convertPoint(g_pole0[step][0],g_pole0[step][1],im_w, im_h)
+        #p1x, p1y = convertPoint(g_pole1[step][0],g_pole1[step][1],im_w, im_h)
+        bx, by, rx, ry, p0x, p0y, p1x, p1y = calcObjectPosition(g_ball[step],g_robot[step],g_pole0[step],g_pole1[step],im_w,im_h,step)
+
     ax.scatter(bx, by,s=80,c='orange')
 #    ax.scatter(lbx, lby,s=100, c='m')
     ax.scatter(rx, ry,s=120,facecolors='none',edgecolors='black')
